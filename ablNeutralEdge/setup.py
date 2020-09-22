f36f5c4dc930ec9e8afaf69716473df0bdd1b84b
Philip Sakievich <psakiev@sanida.gov>
2020-09-22 15:37:51 -0600

    add timestep changes

--- ablNeutralEdge/setup.py
+++ ablNeutralEdge/setup.py
@@ -6,6 +6,11 @@ if len(sys.argv) > 1:
     config = sys.argv[1].strip().lower()
 
 mesh_size = ["02", "05", "10", "20", "40"]
+time_step = [0.15,  0.3,  0.6,  1.2,  2.4]
+cases = []
+for i in range(len(mesh_size)):
+    cases.append({"ms" : mesh_size[i], "ts" : time_step[i]})
+
 input_file = "input.yaml"
 output_file = "{base}_{ms}m_sgs2_CFL_485.yaml"
 
@@ -16,8 +21,9 @@ else:
     use_actuator = ""
     base_name = "ablNeutralEdge"
 
-for m in mesh_size:
-    command = ["aprepro", "-c#", "mesh_size='{ms}'".format(ms=m),
-                    "use_actuator={ua}".format(ua=use_actuator), input_file, output_file.format(ms=m, base=base_name)]
+for case in cases:
+    command = ["aprepro", "-c#", "mesh_size='{ms}'".format(ms=case["ms"]), "timestep={ts}".format(ts=case["ts"]),
+                    "use_actuator={ua}".format(ua=use_actuator), input_file,
+                    output_file.format(ms=case["ms"], base=base_name)]
     print(command)
     subprocess.run(command)
