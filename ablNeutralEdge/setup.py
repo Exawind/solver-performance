6ab8f08633362718c70eebcc8134e74b414a2d74
Philip Sakievich <psakiev@sanida.gov>
2020-09-22 16:13:06 -0600

    Confirm actuator stuff added

--- ablNeutralEdge/setup.py
+++ ablNeutralEdge/setup.py
@@ -9,21 +9,18 @@ cases
 import sys
 import subprocess
 
-config = ''
-if len(sys.argv) > 1:
-    config = sys.argv[1].strip().lower()
-
 mesh_size = ["02", "05", "10", "20", "40"]
 time_step = [0.15,  0.3,  0.6,  1.2,  2.4]
 cases = []
+
 for i in range(len(mesh_size)):
     cases.append({"ms" : mesh_size[i], "ts" : time_step[i]})
 
 input_file = "input.yaml"
 output_file = "{base}_{ms}m_sgs2_CFL_485.yaml"
 
-if config == "actuator":
-    use_actuator = "T"
+if len(sys.argv) > 1:
+    use_actuator = True
     base_name = "nrel5MWactuatorLine"
 else:
     use_actuator = ""
